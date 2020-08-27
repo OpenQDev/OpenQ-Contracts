d274a6bd87f50e3cfa4864ef420f1b6de3b17e36
mktcode <kontakt@markus-kottlaender.de>
2020-08-27 14:48:28 +0200

    added registration note in withdraw box

--- pages/index.vue
+++ pages/index.vue
@@ -41,16 +41,25 @@
             <InfoIcon v-tooltip="'Paste the URL of the GitHub issue or pull request you want to withdraw from.'" width="18px" height="18px" class="mb-1 info-icon" />
           </small>
           <input type="text" class="form-control form-control-lg mb-2" placeholder="https://github.com/..." />
-          <small class="text-muted d-flex justify-content-between">
-            Amount to withdraw
-          </small>
-          <div class="amount-input mb-2">
-            <input type="number" min="0" step="0.01" class="form-control form-control-lg mb-2" placeholder="0.00" />
-            <span>ETH</span>
-          </div>
-          <a href="#" class="btn btn-lg btn-primary shadow-sm d-block mt-4">
+          <a href="#" class="btn btn-lg btn-primary shadow-sm d-block mt-4" v-if="registered">
             Withdraw
           </a>
+          <div v-else>
+            <div class="alert alert-primary border-0 mt-4">
+              <small>
+                You are trying to withdraw a deposit for a merged pull request.
+                To be eligible for this withdrawal you need to verify your GitHub
+                account by creating a repository named after your ethereum address.<br>
+                <div class="d-flex justify-content-between border border-primary rounded-lg px-2 py-1 mt-2">
+                  <i class="my-auto">github.com/mktcode/0x27711...9E520</i>
+                  <span class="p-1"><font-awesome-icon :icon="['far', 'copy']" /></span>
+                </div>
+              </small>
+            </div>
+            <a href="#" class="btn btn-lg btn-primary shadow-sm d-block mt-4" @click="registered = true">
+              Register
+            </a>
+          </div>
         </div>
       </div>
     </transition>
@@ -62,7 +71,8 @@ export default {
   transition: 'fade',
   data() {
     return {
-      view: 'deposit'
+      view: 'deposit',
+      registered: false,
     }
   },
 }
