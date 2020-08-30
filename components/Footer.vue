5fa7dbdf4f44d1c66d52191181262ba552cdee3b
mktcode <kontakt@markus-kottlaender.de>
2020-08-30 12:39:51 +0200

    added footer

---
+++ components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer class="mt-3">
+    <a href="#" class="text-muted px-2">
+      <font-awesome-icon :icon="['fab', 'twitter']" />
+    </a>
+    <a href="#" class="text-muted px-2">
+      <font-awesome-icon :icon="['fab', 'github']" />
+    </a>
+    <a href="#" class="text-muted px-2">
+      <font-awesome-icon :icon="['fab', 'ethereum']" />
+    </a>
+  </footer>
+</template>
+
+<style lang="sass" scoped>
+footer
+  a
+    font-size: 1.2rem
+    opacity: 0.5
+    &:hover
+      opacity: 1
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -2,5 +2,6 @@
   <div class="container main d-flex flex-column align-items-center mb-5">
     <Header />
     <Nuxt />
+    <Footer />
   </div>
 </template>
