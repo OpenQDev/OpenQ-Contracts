f92b7b5329dc666658e406a95572f2ef429668f3
mktcode <kontakt@markus-kottlaender.de>
2020-08-30 16:52:31 +0200

    added component to display shortened addresses

---
+++ components/AddressShort.vue
@@ -0,0 +1,14 @@
+<template>
+  <span>{{ shortenedAddress }}</span>
+</template>
+
+<script>
+export default {
+  props: ['address'],
+  computed: {
+    shortenedAddress() {
+      return this.address.substr(0, 5) + '...' + this.address.substr(38)
+    }
+  }
+}
+</script>
