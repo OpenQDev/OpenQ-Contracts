4bee59f4fa0bdb370a6ee507400199b9f729a8b4
mktcode <kontakt@markus-kottlaender.de>
2020-09-04 22:49:56 +0200

    more space below footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,14 +1,16 @@
 <template>
   <footer class="pt-3 pb-5">
-    <a href="#" class="text-muted px-2">
-      <font-awesome-icon :icon="['fab', 'twitter']" />
-    </a>
-    <a href="#" class="text-muted px-2">
-      <font-awesome-icon :icon="['fab', 'github']" />
-    </a>
-    <a href="#" class="text-muted px-2">
-      <font-awesome-icon :icon="['fab', 'ethereum']" />
-    </a>
+    <div class="mb-5">
+      <a href="#" class="text-muted px-2">
+        <font-awesome-icon :icon="['fab', 'twitter']" />
+      </a>
+      <a href="#" class="text-muted px-2">
+        <font-awesome-icon :icon="['fab', 'github']" />
+      </a>
+      <a href="#" class="text-muted px-2">
+        <font-awesome-icon :icon="['fab', 'ethereum']" />
+      </a>
+    </div>
   </footer>
 </template>
 
