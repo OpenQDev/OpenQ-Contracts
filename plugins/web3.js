db874fcf488dca089dab9722f30de2849f51723a
mktcode <kontakt@markus-kottlaender.de>
2020-10-31 17:12:54 +0100

    check for window.ethereum

--- plugins/web3.js
+++ plugins/web3.js
@@ -4,13 +4,15 @@ export default ({ app }, inject) => {
   const web3 = typeof window.ethereum === 'undefined' ? null : new Web3(Web3.givenProvider || "ws://localhost:8545");
   const mergePay = web3 ? new web3.eth.Contract(process.env.MERGEPAY_ABI, process.env.MERGEPAY_ADDRESS) : null
 
-  window.ethereum.on('accountsChanged', accounts => {
-    app.store.dispatch('load')
-  })
+  if (window.ethereum) {
+    window.ethereum.on('accountsChanged', accounts => {
+      app.store.dispatch('load')
+    })
 
-  window.ethereum.on('networkChanged', network => {
-    app.store.dispatch('load')
-  })
+    window.ethereum.on('networkChanged', network => {
+      app.store.dispatch('load')
+    })
+  }
 
   inject('mergePay', mergePay)
   inject('web3', web3)
