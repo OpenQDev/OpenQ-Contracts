f47ba5d9b5d6fb4c485f9b7ae6351bfcfea01878
mktcode <kontakt@markus-kottlaender.de>
2020-11-02 13:00:40 +0100

    remove header's top border

--- components/Header.vue
+++ components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="header mb-5 pb-3 pt-3 px-3 border">
+    <div class="header mb-5 pb-3 pt-3 px-3 border border-top-0">
       <Logo class="d-flex justify-content-center" />
       <CompatNote />
       <Connect class="mt-3" />
