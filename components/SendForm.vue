3ad7c9f6e36ba2d8a586e60e43c89a9b3f9ae101
mktcode <kontakt@markus-kottlaender.de>
2020-11-05 02:44:04 +0100

    empty username input button

--- components/SendForm.vue
+++ components/SendForm.vue
@@ -12,6 +12,11 @@
     </small>
     <div class="input-with-embed">
       <input type="text" class="form-control form-control-lg form-control-with-embed mb-2" v-model="username" />
+      <a href="#" class="position-absolute text-muted-light" style="top: 12px; right: 50px; z-index: 2" v-if="user" @click="username = ''">
+        <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+          <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
+        </svg>
+      </a>
       <div v-if="loading || user">
         <font-awesome-icon :icon="['fas', 'circle-notch']" spin v-if="loading" class="text-muted-light" />
         <UserEmbed :user="user" :address="address" v-if="user" />
