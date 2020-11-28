befae9ade677e155af2b3eb4ea4d931840dd29a3
mktcode <kontakt@markus-kottlaender.de>
2020-11-28 22:35:09 +0100

    made logo less opaque

--- layouts/about.vue
+++ layouts/about.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container main d-flex flex-column align-items-center p-5">
-    <Logo class="mt-5 d-flex justify-content-center" color="white" size="xl" style="opacity: 0.5" />
+    <Logo class="mt-5 d-flex justify-content-center" color="white" size="xl" style="opacity: 0.8" />
     <Nuxt />
     <Footer />
   </div>
