2466cf068997a38e06b576c8273e17a9f918ab3e
mktcode <kontakt@markus-kottlaender.de>
2020-11-29 04:25:31 +0100

    changed warning color

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container-fluid main d-flex flex-column align-items-center pb-5">
-    <div class="alert alert-danger border-0 mt-3" v-if="showWarning" style="max-width: 480px">
+    <div class="alert alert-info border-0 mt-3" v-if="showWarning" style="max-width: 480px">
       <button type="button" class="close" @click="showWarning = false">
         <svg style="width:24px;height:24px" viewBox="0 0 24 24">
           <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
