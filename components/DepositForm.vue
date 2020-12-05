8b3819a2c94face545e1c41e08690a93dbcf0fa6
mktcode <kontakt@markus-kottlaender.de>
2020-12-05 19:49:41 +0100

    made deposit button bigger

--- components/DepositForm.vue
+++ components/DepositForm.vue
@@ -6,7 +6,7 @@
           <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
         </svg>
       </a>
-      <a href="#" class="btn btn-block btn-primary shadow-sm" @click="open = !open" v-else>
+      <a href="#" class="btn btn-block btn-lg btn-primary shadow-sm" @click="open = !open" v-else>
         Deposit
       </a>
     </div>
