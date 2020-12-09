40412333ee2380dc1f19cf9c274cc620a1d5ac58
mktcode <kontakt@markus-kottlaender.de>
2020-12-09 04:47:39 +0100

    don't show logo on main app page

--- components/Header.vue
+++ components/Header.vue
@@ -2,6 +2,5 @@
   <div class="header mb-3 pb-3 pt-3 px-3">
     <Connect />
     <CompatNote />
-    <Logo class="mt-5 d-flex justify-content-center" color="white" size="xl" style="opacity: 0.8" />
   </div>
 </template>
