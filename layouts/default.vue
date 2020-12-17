2c654944a79f52db7bc951498e78f6d0227afbdc
mktcode <kontakt@markus-kottlaender.de>
2020-12-17 02:09:52 +0100

    changed alert title

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,7 @@
           <path fill="currentColor" d="M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z" />
         </svg>
       </button>
-      <font-awesome-icon :icon="['fas', 'exclamation-triangle']" /> <b>This project is experimental!</b>
+      <font-awesome-icon :icon="['fas', 'exclamation-triangle']" /> <b>This is a prototype!</b>
       <div class="mt-2 d-flex flex-column">
         <div>
           Please connect to the <b>Kovan</b> test network, try things out (spot the bugs) and share your thoughts.<br>
