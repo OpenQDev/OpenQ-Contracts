010bbaf7c15baf6cabfb8132e38dc674d1ab6b38
mktcode <kontakt@markus-kottlaender.de>
2020-12-20 02:34:27 +0100

    removed old oracle api scripts

--- api/index.js
+++ api/index.js
@@ -5,9 +5,6 @@ import githubUserController from "./controllers/github-user";
 import githubIssueController from "./controllers/github-issue";
 import githubIssueByIdController from "./controllers/github-issue-by-id";
 import githubPullRequestController from "./controllers/github-pullrequest";
-import oracleRegisterController from "./controllers/oracle/register"
-import oracleClaimController from "./controllers/oracle/claim"
-import oracleReleaseController from "./controllers/oracle/release"
 
 const app = express();
 
@@ -18,10 +15,6 @@ app.get("/github-user/:username", githubUserController);
 app.get("/github-issue/:owner/:repo/:number", githubIssueController);
 app.get("/github-issue-by-id/:issueId", githubIssueByIdController);
 app.get("/github-pullrequest/:owner/:repo/:number", githubPullRequestController);
-
-app.get("/oracle/register/:githubUser/:ethAddress", oracleRegisterController)
-app.get("/oracle/claim/:githubUser/:prId", oracleClaimController)
-app.get("/oracle/release/:githubUser/:issueId", oracleReleaseController)
 
 module.exports = {
   path: "/api",
