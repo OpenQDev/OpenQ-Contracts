411254c05f4ff9f0654eb9684daf194ae2cb26bb
mktcode <kontakt@markus-kottlaender.de>
2020-12-20 03:06:47 +0100

    implemented chainlink into contract (compiles at least)

--- contracts/OctoBay.sol
+++ contracts/OctoBay.sol
@@ -7,83 +7,98 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.6/ChainlinkClient.sol";
 
 contract OctoBay is ERC20, Ownable, ChainlinkClient {
+  event UserDepositEvent(address account, uint256 amount, string githubUser);
+  event UserSendEvent(address account, uint256 amount, string githubUser);
+  event IssueDepositEvent(address account, uint256 amount, string issueId);
+  event ReleaseIssueDepositsEvent(string issueId, string githubUser);
+
   struct User {
-    address account;
-    bool confirmed;
+    string githubUser;
+    address ethAddress;
+    uint256 status; // 1 = requested, 2 = confirmed
   }
+  mapping(bytes32 => User) public users;
+  mapping(address => bytes32) public userIDsByAddress;
+  mapping(string => bytes32) public userIDsByGithubUser;
 
   struct UserDeposit {
     address from;
     uint256 amount;
     string githubUser;
   }
+  uint256 private nextUserDepositId = 0;
+  mapping(uint256 => UserDeposit) public userDeposits;
+  mapping(string => uint256[]) public userDepositIdsByGithubUser;
+  mapping(address => uint256[]) public userDepositIdsBySender;
 
   struct IssueDeposit {
     address from;
     uint256 amount;
     string issueId;
   }
-
-  event RegistrationRequestEvent(address account, string githubUser);
-  event RegistrationConfirmedEvent(address account, string githubUser);
-  event ClaimPrRequestEvent(string prId, string githubUser);
-  event ClaimPrConfirmEvent(string prId, string githubUser, uint256 score);
-  event UserDepositEvent(address account, uint256 amount, string githubUser);
-  event UserSendEvent(address account, uint256 amount, string githubUser);
-  event IssueDepositEvent(address account, uint256 amount, string issueId);
-  event ReleaseIssueDepositsRequestEvent(string issueId, string githubUser, string owner);
-  event ReleaseIssueDepositsConfirmEvent(string issueId, string githubUser, string owner);
-
-  mapping(string => User) public users;
-  mapping(address => string) public usersByAddress;
-
-  mapping(uint256 => UserDeposit) public userDeposits;
-  uint256 private nextUserDepositId = 0;
-  mapping(string => uint256[]) public userDepositIdsByGithubUser;
-  mapping(address => uint256[]) public userDepositIdsBySender;
-
   mapping(uint256 => IssueDeposit) public issueDeposits;
   uint256 public nextIssueDepositId = 0;
   mapping(string => uint256[]) public issueDepositIdsByIssueId;
   mapping(address => uint256[]) public issueDepositIdsBySender;
+  mapping(string => uint256) public issueDepositsAmountByIssueId;
 
-  mapping(string => string) public releasedIssues;
-  mapping(string => string) public releasedIssueRequests;
-  mapping(string => string) public releasedIssueRequestsOwnedBy;
+  struct ReleasedIssue {
+    string githubUser;
+    string issueId;
+    uint256 status;
+  }
+  mapping(bytes32 => ReleasedIssue) public releasedIssues;
+  mapping(string => bytes32) public issueReleaseIDsByIssueId;
 
-  mapping(string => uint256) public issueBoosts;
-  mapping(string => uint8) public claimedPullRequests;
+  struct PullRequestClaim {
+    string githubUser;
+    string prId;
+    uint256 status; // 1 = requested, 2 = confirmed
+  }
+  mapping(bytes32 => PullRequestClaim) public pullRequestClaims;
+  mapping(string => bytes32) public pullRequestClaimIDsByPrId;
+
+  mapping(string => uint256) public issuePins;
+
+  uint256 private registrationFee;
+  uint256 private claimFee;
 
   constructor() ERC20("OctoPin", "OPIN") public {
     setPublicChainlinkToken();
+    registrationFee = 0.1 * 10 ** 18; // 0.1 LINK
+    claimFee = 0.1 * 10 ** 18; // 0.1 LINK
   }
 
-  function register(string calldata _githubUser) external payable {
-    if (users[_githubUser].account != address(0)) {
-      delete usersByAddress[users[_githubUser].account];
-    }
-    users[_githubUser] = User(msg.sender, false);
-    usersByAddress[msg.sender] = _githubUser;
+  function register(address _oracle, bytes32 _jobId, string memory _githubUser) public {
+    Chainlink.Request memory request = buildChainlinkRequest(_jobId, address(this), this.confirmRegistration.selector);
+    request.add("githubUser", _githubUser);
+    request.addBytes("ethAccount", abi.encodePacked(msg.sender));
+    bytes32 requestId = sendChainlinkRequestTo(_oracle, request, registrationFee);
 
-    payable(owner()).transfer(msg.value);
-
-    emit RegistrationRequestEvent(msg.sender, _githubUser);
+    users[requestId] = User(_githubUser, msg.sender, 1);
   }
 
-  function registerConfirm(string calldata _githubUser, address account) external {
-    require(
-      users[_githubUser].account != address(0) && users[_githubUser].account == account,
-      "This account confirmation was never requested."
-    );
-    users[_githubUser].confirmed = true;
-    emit RegistrationConfirmedEvent(account, _githubUser);
+  function confirmRegistration(bytes32 _requestId) public recordChainlinkFulfillment(_requestId) {
+    users[_requestId].status == 2;
+    userIDsByAddress[users[_requestId].ethAddress] = _requestId;
+    userIDsByGithubUser[users[_requestId].githubUser] = _requestId;
+
+    // If githubUser was registered before (changed ethAddress now) look for old
+    // ID and delete entry in userIDsByAddress as well as users entry itself (userIDsByGithubUser
+    // has been overridden above anyway).
+    bytes32 oldId = userIDsByGithubUser[users[_requestId].githubUser];
+    delete userIDsByAddress[users[oldId].ethAddress];
+    delete users[oldId];
+
+    // The scenario where an eth address switches to a new github account
+    // (the other way around) does not occur.
   }
 
   function sendEthToGithubUser(string calldata _githubUser) external payable {
     require(msg.value > 0, "You must send ETH.");
-    require(users[_githubUser].account != address(0), "This GitHub User is not registered.");
-    require(users[_githubUser].confirmed == true, "This GitHub User is not confirmed yet.");
-    payable(users[_githubUser].account).transfer(msg.value);
+    require(users[userIDsByGithubUser[_githubUser]].ethAddress != address(0), "This GitHub user is not registered.");
+    require(users[userIDsByGithubUser[_githubUser]].status == 2, "This GitHub user is not confirmed yet.");
+    payable(users[userIDsByGithubUser[_githubUser]].ethAddress).transfer(msg.value);
     emit UserSendEvent(msg.sender, msg.value, _githubUser);
   }
 
@@ -97,14 +112,14 @@ contract OctoBay is ERC20, Ownable, ChainlinkClient {
   }
 
   function withdrawUserDeposit(uint256 _depositId) external {
-    require(users[usersByAddress[msg.sender]].account == msg.sender, "GitHub account not registered with this Ethereum account.");
-    require(keccak256(bytes(userDeposits[_depositId].githubUser)) == keccak256(bytes(usersByAddress[msg.sender])), "Deposit is not for this GitHub account.");
+    require(users[userIDsByAddress[msg.sender]].ethAddress == msg.sender, "This Ethereum address is not registered with any GitHub user.");
+    require(keccak256(bytes(userDeposits[_depositId].githubUser)) == keccak256(bytes(users[userIDsByAddress[msg.sender]].githubUser)), "Deposit is not for this GitHub account.");
     payable(msg.sender).transfer(userDeposits[_depositId].amount);
     delete userDeposits[_depositId];
   }
 
   function refundUserDeposit(uint256 _depositId) external {
-    require(userDeposits[_depositId].from == msg.sender, "Deposit did not came from this Ethereum account.");
+    require(userDeposits[_depositId].from == msg.sender, "Deposit did not came from this Ethereum address.");
     payable(msg.sender).transfer(userDeposits[_depositId].amount);
     delete userDeposits[_depositId];
   }
@@ -123,12 +138,15 @@ contract OctoBay is ERC20, Ownable, ChainlinkClient {
     issueDeposits[nextIssueDepositId] = IssueDeposit(msg.sender, msg.value, _issueId);
     issueDepositIdsByIssueId[_issueId].push(nextIssueDepositId);
     issueDepositIdsBySender[msg.sender].push(nextIssueDepositId);
+    issueDepositsAmountByIssueId[_issueId] += msg.value;
     emit IssueDepositEvent(msg.sender, msg.value, _issueId);
   }
 
   function refundIssueDeposit(uint256 _depositId) external {
-    require(issueDeposits[_depositId].from == msg.sender, "Deposit did not came from this Ethereum account.");
+    require(issueDeposits[_depositId].from == msg.sender, "Deposit did not come from this Ethereum address or does not exist.");
+    require(issueDepositsAmountByIssueId[issueDeposits[_depositId].issueId] >= issueDeposits[_depositId].amount, "This issue deposit has been withdrawn already.");
     payable(msg.sender).transfer(issueDeposits[_depositId].amount);
+    issueDepositsAmountByIssueId[issueDeposits[_depositId].issueId] -= issueDeposits[_depositId].amount;
     delete issueDeposits[_depositId];
   }
 
@@ -140,57 +158,58 @@ contract OctoBay is ERC20, Ownable, ChainlinkClient {
     return issueDepositIdsBySender[msg.sender];
   }
 
-  function pinIssue(string memory _depositId, uint256 _amount) public {
+  function pinIssue(string memory _issueId, uint256 _amount) public {
     require(_amount > 0, "Amount must be greater zero.");
     _burn(msg.sender, _amount);
-    issueBoosts[_depositId] += _amount;
+    issuePins[_issueId] += _amount;
   }
 
-  function claimPullRequest(string calldata _prId, string calldata _githubUser) external payable {
-    require(claimedPullRequests[_prId] != 2, "Pull request already claimed.");
-    require(users[_githubUser].account != address(0), "This GitHub User is not registered.");
-    payable(owner()).transfer(msg.value);
-    claimedPullRequests[_prId] = 1;
-    emit ClaimPrRequestEvent(_prId, _githubUser);
+  function claimPullRequest(address _oracle, bytes32 _jobId, string memory _prId, string memory _githubUser) public {
+    require(pullRequestClaims[pullRequestClaimIDsByPrId[_prId]].status != 2, "Pull request already claimed.");
+    require(users[userIDsByGithubUser[_githubUser]].status != 2, "This GitHub user is not registered.");
+
+    Chainlink.Request memory request = buildChainlinkRequest(_jobId, address(this), this.confirmPullRequestClaim.selector);
+    request.add("githubUser", _githubUser);
+    request.add("prId", _prId);
+    bytes32 requestId = sendChainlinkRequestTo(_oracle, request, claimFee);
+
+    pullRequestClaims[requestId] = PullRequestClaim(_githubUser, _prId, 1);
   }
 
-  function confirmClaimPullRequest(string calldata _prId, string calldata _githubUser, uint256 _score) external {
+  function confirmPullRequestClaim(bytes32 _requestId, uint256 _score) public recordChainlinkFulfillment(_requestId) {
     require(1 <= _score && _score <= 100, "Invalid score: 1 <= score <= 100");
-    require(claimedPullRequests[_prId] == 1, "Pull request already claimed.");
-    require(users[_githubUser].account != address(0), "This GitHub User is not registered.");
-    claimedPullRequests[_prId] = 2;
-    _mint(users[_githubUser].account, _score * uint256(10) ** decimals());
-    emit ClaimPrConfirmEvent(_prId, _githubUser, _score);
+    require(pullRequestClaims[_requestId].status == 1, "Pull request claim was not requested.");
+    require(users[userIDsByGithubUser[pullRequestClaims[_requestId].githubUser]].ethAddress != address(0), "This GitHub user is not registered.");
+
+    pullRequestClaims[_requestId].status = 2;
+    _mint(users[userIDsByGithubUser[pullRequestClaims[_requestId].githubUser]].ethAddress, _score * uint256(10) ** decimals());
   }
 
-  function releaseIssueDeposits(string calldata _issueId, string calldata _githubUser) external payable {
-    require(issueDepositIdsByIssueId[_issueId].length > 0, "Issue has no deposits to release.");
-    require(users[usersByAddress[msg.sender]].account == msg.sender, "No GitHub account registered with this Ethereum account.");
-    payable(owner()).transfer(msg.value);
-    releasedIssueRequests[_issueId] = _githubUser;
-    releasedIssueRequestsOwnedBy[_issueId] = usersByAddress[msg.sender];
-    emit ReleaseIssueDepositsRequestEvent(_issueId, _githubUser, usersByAddress[msg.sender]);
+  function releaseIssueDeposits(address _oracle, bytes32 _jobId, string memory _issueId, string memory _githubUser) public {
+    require(issueDepositsAmountByIssueId[_issueId] > 0, "Issue has no deposits to release.");
+    require(users[userIDsByAddress[msg.sender]].ethAddress == msg.sender, "Only registered GitHub users can release issue deposits.");
+
+    Chainlink.Request memory request = buildChainlinkRequest(_jobId, address(this), this.confirmReleaseIssueDeposits.selector);
+    request.add("githubUser", _githubUser);
+    request.add("issueId", _issueId);
+    bytes32 requestId = sendChainlinkRequestTo(_oracle, request, registrationFee);
+
+    releasedIssues[requestId] = ReleasedIssue(_githubUser, _issueId, 1);
+    issueReleaseIDsByIssueId[_issueId] = requestId;
   }
 
-  function confirmReleaseIssueDeposits(string calldata _issueId, string calldata _githubUser) external {
-    require(issueDepositIdsByIssueId[_issueId].length > 0, "Issue has no deposits to release.");
-    require(keccak256(bytes(releasedIssueRequests[_issueId])) == keccak256(bytes(_githubUser)), "This issue release was not requested.");
-    emit ReleaseIssueDepositsConfirmEvent(_issueId, _githubUser, releasedIssueRequestsOwnedBy[_issueId]);
-    releasedIssues[_issueId] = _githubUser;
-    delete releasedIssueRequests[_issueId];
-    delete releasedIssueRequestsOwnedBy[_issueId];
+  function confirmReleaseIssueDeposits(bytes32 _requestId) public recordChainlinkFulfillment(_requestId) {
+    require(releasedIssues[_requestId].status == 1, "Isse deposit release was not requested.");
+    releasedIssues[_requestId].status = 2;
+    emit ReleaseIssueDepositsEvent(releasedIssues[_requestId].issueId, releasedIssues[_requestId].githubUser);
   }
 
   function claimReleasedIssueDeposits(string calldata _issueId) external {
-    require(users[usersByAddress[msg.sender]].account == msg.sender, "No GitHub account registered with this Ethereum account.");
-    require(keccak256(bytes(releasedIssues[_issueId])) == keccak256(bytes(usersByAddress[msg.sender])), "The GitHub account this issue was released to does not belong to this Ethereum account.");
+    require(users[userIDsByAddress[msg.sender]].ethAddress == msg.sender, "No GitHub account registered with this Ethereum account.");
+    require(keccak256(bytes(releasedIssues[issueReleaseIDsByIssueId[_issueId]].githubUser)) == keccak256(bytes(users[userIDsByAddress[msg.sender]].githubUser)), "The GitHub account this issue was released to does not belong to this Ethereum account.");
+    require(issueDepositsAmountByIssueId[_issueId] > 0, "Issue deposits already claimed.");
 
-    for (uint256 i = 0; i < issueDepositIdsByIssueId[_issueId].length; i++) {
-      if (issueDeposits[issueDepositIdsByIssueId[_issueId][i]].amount > 0) {
-        payable(msg.sender).transfer(issueDeposits[issueDepositIdsByIssueId[_issueId][i]].amount);
-      }
-      delete issueDeposits[issueDepositIdsByIssueId[_issueId][i]];
-    }
-    delete releasedIssues[_issueId];
+    payable(msg.sender).transfer(issueDepositsAmountByIssueId[_issueId]);
+    issueDepositsAmountByIssueId[_issueId] = 0;
   }
 }
