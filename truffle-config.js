47ab1cec19b830cecb169fc43ae7fbed9f3cd840
mktcode <kontakt@markus-kottlaender.de>
2021-01-06 20:18:16 +0100

    use kovan for deployment

--- truffle-config.js
+++ truffle-config.js
@@ -58,7 +58,7 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     kovan: {
-      provider: () => new HDWalletProvider(mnemonic, `wss://eth-kovan.ws.alchemyapi.io/v2/${process.env.DEPLOYMENT_API_KEY}` ),
+      provider: () => new HDWalletProvider(mnemonic, `wss://kovan.infura.io/ws/v3/${process.env.KOVAN_API_KEY}` ),
       network_id: 42,       // Ropsten's id
       gas: 5500000,        // Ropsten has a lower block limit than mainnet
       confirmations: 2,    // # of confs to wait between deployments. (default: 0)
