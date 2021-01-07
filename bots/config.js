c990ba76d1cbbc321e0f8d5f20ee6ba401c8a7a7
mktcode <kontakt@markus-kottlaender.de>
2021-01-07 11:16:28 +0100

    no need for wallet in bots anymore

--- bots/config.js
+++ bots/config.js
@@ -4,10 +4,7 @@ const axios = require("axios")
 const Web3 = require('web3')
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const fs = require('fs');
-const mnemonic = fs.readFileSync(".mnemonic").toString().trim();
 const web3 = new Web3(process.env.ETH_NODE)
-const walletProvider = new HDWalletProvider(mnemonic, process.env.ETH_NODE)
-const web3wallet = new Web3(walletProvider)
 const octobay = new web3wallet.eth.Contract(require('./../OctoBay.json').abi, process.env.OCTOBAY_ADDRESS)
 
-module.exports = { web3, web3wallet, octobay, axios, fs }
+module.exports = { web3, octobay, axios, fs }
