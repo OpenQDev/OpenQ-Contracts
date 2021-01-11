20f4776713e5a49e806a7e8b8948b436fd02eab3
mktcode <kontakt@markus-kottlaender.de>
2021-01-11 22:41:27 +0100

    added tests

--- evm/test/octobay.js
+++ evm/test/octobay.js
@@ -1,50 +1,76 @@
-const fs = require('fs')
 const OctoBay = artifacts.require("OctoBay")
+const OctoPin = artifacts.require("OctoPin")
 const Oracle = artifacts.require("Oracle")
-const githubUser = 'mktcode'
+const LinkToken = artifacts.require("LinkToken")
+const someAddress = "0xE781857C5b55ff0551Df167D0B0A4C53BFD08e1D"
+const someGithubUser = "mktcode"
 
 contract("OctoBay", async accounts => {
-  let oracle;
-
-  it(`Check correct oracle and jobIDs were set in Octobay`, async () => {
-    const octoBayInstance = await OctoBay.deployed()
-    const oracleInstance = await Oracle.deployed()
-    oracle = await octoBayInstance.activeOracles(oracleInstance.address)
-
-    const registerJobExists = fs.statSync(`./../../chainlink/.jobs/register.${web3.utils.hexToAscii(oracle.registerJobId)}.json`)
-    const releaseJobExists = fs.statSync(`./../../chainlink/.jobs/release.${web3.utils.hexToAscii(oracle.releaseJobId)}.json`)
-    const claimJobExists = fs.statSync(`./../../chainlink/.jobs/claim.${web3.utils.hexToAscii(oracle.claimJobId)}.json`)
-    const graphqlBoolJobExists = fs.statSync(`./../../chainlink/.jobs/graphql/bool.${web3.utils.hexToAscii(oracle.graphqlBoolJobId)}.json`)
-    const graphqlBytes32JobExists = fs.statSync(`./../../chainlink/.jobs/graphql/bytes32.${web3.utils.hexToAscii(oracle.graphqlBytes32JobId)}.json`)
-    const graphqlInt256JobExists = fs.statSync(`./../../chainlink/.jobs/graphql/int256.${web3.utils.hexToAscii(oracle.graphqlInt256JobId)}.json`)
-    const graphqlUint256JobExists = fs.statSync(`./../../chainlink/.jobs/graphql/uint256.${web3.utils.hexToAscii(oracle.graphqlUint256JobId)}.json`)
-
-    // assert.equal(registerJobExists, true, 'Register job correct.')
-    // assert.equal(releaseJobExists, true, 'Release job correct.')
-    // assert.equal(claimJobExists, true, 'Claim job correct.')
-    // assert.equal(graphqlBoolJobExists, true, 'GraphQL Bool job correct.')
-    // assert.equal(graphqlBytes32JobExists, true, 'GraphQL Bytes32 job correct.')
-    // assert.equal(graphqlInt256JobExists, true, 'GraphQL Int256 job correct.')
-    // assert.equal(graphqlUint256JobExists, true, 'GraphQL Uint256 job correct.')
+  it("sets OctoPin token", async () => {
+    const octobay = await OctoBay.deployed()
+    const octopin = await OctoPin.deployed()
+    await octobay.setOctoPin(octopin.address)
+    const newAddress = await octobay.octoPin()
+    assert.equal(octopin.address, newAddress)
   })
 
-  it(`${githubUser} should be unregistered`, async () => {
-    const instance = await OctoBay.deployed()
-    const userId = await instance.userIDsByGithubUser(githubUser)
-    assert.equal(
-      userId,
-      0,
-      `${githubUser} is already registered`
+  it("sets Oracle", async () => {
+    const octobay = await OctoBay.deployed()
+    await octobay.setOracle(
+      someAddress,
+      "Main",
+      web3.utils.toHex("0"),
+      web3.utils.toHex("0"),
+      web3.utils.toHex("0"),
+      web3.utils.toHex("0"),
+      web3.utils.toHex("0"),
+      web3.utils.toHex("0"),
+      web3.utils.toHex("0")
     )
+    const oracles = await octobay.getOracles()
+    assert.equal(oracles.includes(someAddress), true)
   })
 
-  it("registration should get rejected due to insufficiennt LINK balance", async () => {
-    const instance = await OctoBay.deployed()
-    const error = await instance.register(oracle.address, web3.utils.toHex(oracle.registerJobId), 'mktcode').catch(e => e)
-    assert.equal(
-      error.reason,
-      'ERC20: transfer amount exceeds balance',
-      "registration was accepted, LINK balance was > 0"
-    )
+  it("removes Oracle", async () => {
+    const octobay = await OctoBay.deployed()
+    await octobay.removeOracle(someAddress)
+    const oracles = await octobay.getOracles()
+    assert.equal(oracles.includes(someAddress), false)
   })
+
+  it("sets Oracle fulfill permission", async () => {
+    const oracle = await Oracle.deployed()
+    const tx = await oracle.setFulfillmentPermission(accounts[0], true)
+    assert.notEqual(tx, null)
+  })
+
+  it("receives LINK token", async () => {
+    const octobay = await OctoBay.deployed()
+    const link = await LinkToken.deployed()
+    const transferBalance = '10000000000000000000'
+    await link.transfer(octobay.address, transferBalance)
+    const balance = await link.balanceOf(octobay.address)
+    assert.equal(balance.toString(), transferBalance)
+  })
+
+  let registerRequestId
+  it("registers a user", async () => {
+    const octobay = await OctoBay.deployed()
+    const oracles = await octobay.getOracles()
+    const oracle = await octobay.activeOracles(oracles[0])
+    const registerRequest = await octobay.register(oracles[0], oracle.registerJobId, someGithubUser)
+    registerRequestId = registerRequest.logs[0].args.id
+    const user = await octobay.users(registerRequestId)
+
+    assert.equal(user.status.toString(), '1')
+  })
+
+  // it("confirms the user", async () => {
+  //   const octobay = await OctoBay.deployed()
+  //   const oracle = await Oracle.deployed()
+  //   await octobay.confirmRegistration(registerRequestId, { from: accounts[0] })
+  //   const user = await octobay.users(registerRequestId)
+  //
+  //   assert.equal(user.status.toString(), '2')
+  // })
 })
