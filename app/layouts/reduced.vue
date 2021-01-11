d0b7b86dfec51708365d6f9fa29cf476fda03318
mktcode <kontakt@markus-kottlaender.de>
2021-01-11 19:17:39 +0100

    removed old file

--- app/layouts/reduced.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="overflow-hidden">
-    <div class="container-fluid main d-flex flex-column align-items-center pb-5">
-      <Header />
-      <Nuxt />
-      <Footer />
-    </div>
-  </div>
-</template>
