9478cf7bd3b247a3da2f98dfd6bd4ca60a935d87
mktcode <kontakt@markus-kottlaender.de>
2021-01-15 11:17:59 +0100

    fund paymaster

--- migrations/6_paymaster.js
+++ migrations/6_paymaster.js
@@ -2,11 +2,12 @@ require("dotenv").config({ path: './../.env' })
 const OctoBay = artifacts.require("OctoBay")
 const OctoBayPaymaster = artifacts.require("OctoBayPaymaster")
 
-module.exports = function (deployer) {
+module.exports = function (deployer, network, accounts) {
   deployer.deploy(OctoBayPaymaster, OctoBay.address).then(paymasterInstance => {
     paymasterInstance.setRelayHub(process.env.GSN_RELAYHUB_ADDRESS)
     OctoBay.deployed().then(octoBayInstance => {
       octoBayInstance.setPaymaster(OctoBayPaymaster.address)
+      web3.eth.sendTransaction({ from: accounts[0], to: OctoBayPaymaster.address, value: '1000000000000000000' })
     })
   })
 }
