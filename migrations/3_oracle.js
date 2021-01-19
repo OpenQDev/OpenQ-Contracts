7176428ab058f604b3b65b965ca62a4b69868057
mktcode <kontakt@markus-kottlaender.de>
2021-01-19 19:18:28 +0100

    prepare kovan deployment

--- migrations/3_oracle.js
+++ migrations/3_oracle.js
@@ -8,5 +8,9 @@ module.exports = function (deployer, network, accounts) {
       oracleInstance.setFulfillmentPermission(process.env.CHAINLINK_NODE_ADDRESS, true)
       web3.eth.sendTransaction({ from: accounts[0], to: process.env.CHAINLINK_NODE_ADDRESS, value: '1000000000000000000' })
     })
+  } else if (network == 'kovan') {
+    deployer.deploy(Oracle, '0xa36085F69e2889c224210F603D836748e7dC0088').then(oracleInstance => {
+      oracleInstance.setFulfillmentPermission(process.env.CHAINLINK_NODE_ADDRESS, true)
+    })
   }
 }
--- migrations/4_octobay.js
+++ migrations/4_octobay.js
@@ -46,9 +46,40 @@ module.exports = function (deployer, network, accounts) {
         linkTokenInstance.transfer(octoBayInstance.address, "10000000000000000000")
       })
     })
-  } else {
-    deployer.deploy(OctoBay, zeroAddress, zeroAddress, zeroAddress, process.env.GSN_FORWARDER_ADDRESS).then(octoBayInstance => {
+  } else if (network == 'kovan') {
+    deployer.deploy(OctoBay, '0xa36085F69e2889c224210F603D836748e7dC0088', zeroAddress, zeroAddress, process.env.GSN_FORWARDER_ADDRESS).then(octoBayInstance => {
       octoBayInstance.setTwitterAccountId(process.env.OCTOBAY_TWITTER_ACCOUNT_ID)
+      octoBayInstance.setOracle(Oracle.address, "Main")
+      octoBayInstance.setOracleJob(
+        Oracle.address,
+        1,
+        web3.utils.toHex(process.env.CHAINLINK_REGISTER_JOB_ID),
+        process.env.CHAINLINK_REGISTER_JOB_FEE
+      )
+      octoBayInstance.setOracleJob(
+        Oracle.address,
+        2,
+        web3.utils.toHex(process.env.CHAINLINK_RELEASE_JOB_ID),
+        process.env.CHAINLINK_RELEASE_JOB_FEE
+      )
+      octoBayInstance.setOracleJob(
+        Oracle.address,
+        3,
+        web3.utils.toHex(process.env.CHAINLINK_CLAIM_JOB_ID),
+        process.env.CHAINLINK_CLAIM_JOB_FEE
+      )
+      octoBayInstance.setOracleJob(
+        Oracle.address,
+        4,
+        web3.utils.toHex(process.env.CHAINLINK_TWITTER_POST_JOB_ID),
+        process.env.CHAINLINK_TWITTER_POST_JOB_FEE
+      )
+      octoBayInstance.setOracleJob(
+        Oracle.address,
+        5,
+        web3.utils.toHex(process.env.CHAINLINK_TWITTER_FOLLOWERS_JOB_ID),
+        process.env.CHAINLINK_TWITTER_FOLLOWERS_JOB_FEE
+      )
     })
   }
 }
