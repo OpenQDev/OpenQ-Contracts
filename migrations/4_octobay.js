23ffd57d6b084cd71db0487009d829f8d0ad9b73
Alpha-Guy <alphaguy2019@gmail.com>
2021-01-28 19:11:50 +0530

    Removed setting oracles from migrations file

--- migrations/4_octobay.js
+++ migrations/4_octobay.js
@@ -12,37 +12,6 @@ module.exports = function (deployer, network, accounts) {
   if (network == 'development') {
     deployer.deploy(OctoBay, LinkToken.address, zeroAddress, zeroAddress, zeroAddress).then(octoBayInstance => {
       octoBayInstance.setTwitterAccountId(process.env.OCTOBAY_TWITTER_ACCOUNT_ID)
-      // octoBayInstance.setOracle(Oracle.address, "Main")
-      // octoBayInstance.setOracleJob(
-      //   Oracle.address,
-      //   1,
-      //   web3.utils.toHex(process.env.CHAINLINK_REGISTER_JOB_ID),
-      //   process.env.CHAINLINK_REGISTER_JOB_FEE
-      // )
-      // octoBayInstance.setOracleJob(
-      //   Oracle.address,
-      //   2,
-      //   web3.utils.toHex(process.env.CHAINLINK_RELEASE_JOB_ID),
-      //   process.env.CHAINLINK_RELEASE_JOB_FEE
-      // )
-      // octoBayInstance.setOracleJob(
-      //   Oracle.address,
-      //   3,
-      //   web3.utils.toHex(process.env.CHAINLINK_CLAIM_JOB_ID),
-      //   process.env.CHAINLINK_CLAIM_JOB_FEE
-      // )
-      // octoBayInstance.setOracleJob(
-      //   Oracle.address,
-      //   4,
-      //   web3.utils.toHex(process.env.CHAINLINK_TWITTER_POST_JOB_ID),
-      //   process.env.CHAINLINK_TWITTER_POST_JOB_FEE
-      // )
-      // octoBayInstance.setOracleJob(
-      //   Oracle.address,
-      //   5,
-      //   web3.utils.toHex(process.env.CHAINLINK_TWITTER_FOLLOWERS_JOB_ID),
-      //   process.env.CHAINLINK_TWITTER_FOLLOWERS_JOB_FEE
-      // )
       LinkToken.deployed().then(linkTokenInstance => {
         linkTokenInstance.transfer(octoBayInstance.address, "10000000000000000000")
       })
@@ -50,37 +19,6 @@ module.exports = function (deployer, network, accounts) {
   } else if (network == 'kovan') {
     deployer.deploy(OctoBay, '0xa36085F69e2889c224210F603D836748e7dC0088', zeroAddress, zeroAddress, process.env.GSN_FORWARDER_ADDRESS).then(octoBayInstance => {
       octoBayInstance.setTwitterAccountId(process.env.OCTOBAY_TWITTER_ACCOUNT_ID)
-      // octoBayInstance.setOracle(Oracle.address, "Main")
-      // octoBayInstance.setOracleJob(
-      //   Oracle.address,
-      //   1,
-      //   web3.utils.toHex(process.env.CHAINLINK_REGISTER_JOB_ID),
-      //   process.env.CHAINLINK_REGISTER_JOB_FEE
-      // )
-      // octoBayInstance.setOracleJob(
-      //   Oracle.address,
-      //   2,
-      //   web3.utils.toHex(process.env.CHAINLINK_RELEASE_JOB_ID),
-      //   process.env.CHAINLINK_RELEASE_JOB_FEE
-      // )
-      // octoBayInstance.setOracleJob(
-      //   Oracle.address,
-      //   3,
-      //   web3.utils.toHex(process.env.CHAINLINK_CLAIM_JOB_ID),
-      //   process.env.CHAINLINK_CLAIM_JOB_FEE
-      // )
-      // octoBayInstance.setOracleJob(
-      //   Oracle.address,
-      //   4,
-      //   web3.utils.toHex(process.env.CHAINLINK_TWITTER_POST_JOB_ID),
-      //   process.env.CHAINLINK_TWITTER_POST_JOB_FEE
-      // )
-      // octoBayInstance.setOracleJob(
-      //   Oracle.address,
-      //   5,
-      //   web3.utils.toHex(process.env.CHAINLINK_TWITTER_FOLLOWERS_JOB_ID),
-      //   process.env.CHAINLINK_TWITTER_FOLLOWERS_JOB_FEE
-      // )
     })
   }
 }
