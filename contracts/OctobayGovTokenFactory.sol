14cc3117d84f59b6b289b8ce858186f4dc53c898
Jasper Timm <jasper.timm@ehealthafrica.org>
2021-03-17 18:47:52 +0000

    Add event to track creation of new tokens

--- contracts/OctobayGovTokenFactory.sol
+++ contracts/OctobayGovTokenFactory.sol
@@ -7,12 +7,15 @@ import './OctobayGovToken.sol';
 
 contract OctobayGovTokenFactory is OctobayStorage {
 
+    event NewTokenEvent(string name, string symbol);
+
     /// @param _name Name of the new token
     /// @param _symbol Token Symbol for the new token
     /// @return The address of the new token contract
     function createToken(string memory _name, string memory _symbol) external onlyOctobay returns (OctobayGovToken) {
         OctobayGovToken newToken = new OctobayGovToken(_name, _symbol);
         newToken.setOctobay(msg.sender);
+        emit NewTokenEvent(_name, _symbol);
         return newToken;
     }    
 }
