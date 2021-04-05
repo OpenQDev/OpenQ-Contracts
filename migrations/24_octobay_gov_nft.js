35af553547541ece7c35fe3380656a481ee30a52
mktcode <kontakt@markus-kottlaender.de>
2021-04-05 21:14:01 +0200

    fixed NFT deployment

--- migrations/24_octobay_gov_nft.js
+++ migrations/24_octobay_gov_nft.js
@@ -3,5 +3,5 @@ const OctobayGovNFT = artifacts.require("OctobayGovNFT")
 
 module.exports = function (deployer, network) {
   if (network == 'test') return;
-  deployer.deploy(OctobayGovNFT)
+  deployer.deploy(OctobayGovNFT, "Octobay Governance Permissions", "OGPERM")
 }
