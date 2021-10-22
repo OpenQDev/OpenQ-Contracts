9e320226b8f1d7e3635232c0832fd5b67747ff8a
Andrew <alo42@georgetown.edu>
2021-10-22 12:56:04 -0500

    removes unused imports

--- contracts/Issue.sol
+++ contracts/Issue.sol
@@ -1,8 +1,6 @@
 // contracts/MockToken.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
-import './Issue.sol';
-import './OpenQ.sol';
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 contract Issue {
