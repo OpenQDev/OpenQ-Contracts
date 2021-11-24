cf679165990ef1cc2774a5ed157fa68b834d085c
FlacoJones <andrew@openq.dev>
2021-11-23 22:49:36 -0600

    adds new describe block for testing OpenQ Reverts

--- test/OpenQ.js
+++ test/OpenQ.js
@@ -2,12 +2,19 @@
 const { BigNumber } = require('@ethersproject/bignumber');
 const { expect } = require('chai');
 require('@nomiclabs/hardhat-waffle');
+const truffleAssert = require('truffle-assertions');
 
-describe('OpenQ.sol', () => {
+describe('OpenQ.sol Transactions', () => {
+	let openQ;
+
+	beforeEach(async () => {
+		const OpenQ = await hre.ethers.getContractFactory('OpenQ');
+		openQ = await OpenQ.deploy();
+	});
+
 	it('mintBounty method should create a new open bounty with expected initial metadata', async () => {
 		// ARRANGE
-		const OpenQ = await hre.ethers.getContractFactory('OpenQ');
-		const openQ = await OpenQ.deploy();
+		await openQ.deployed();
 
 		// Manually set timestamp for next block
 		const expectedTimestamp = 1953282725;
@@ -16,14 +23,13 @@ describe('OpenQ.sol', () => {
 		// ACT
 		await openQ.mintBounty('mockIssueId');
 
-		// ASSERT
 		const issueIsOpen = await openQ.issueIsOpen('mockIssueId');
 		const issueAddress = await openQ.issueToAddress('mockIssueId');
 
 		const Issue = await hre.ethers.getContractFactory('Issue');
 
 		const newIssue = await Issue.attach(
-			issueAddress// The deployed contract address
+			issueAddress
 		);
 
 		const issueId = await newIssue.issueId();
@@ -34,6 +40,7 @@ describe('OpenQ.sol', () => {
 		const closer = await newIssue.closer();
 		const status = await newIssue.status();
 
+		// ASSERT
 		expect(issueId).to.equal("mockIssueId");
 		expect(issueCreatedTime).to.equal(expectedTimestamp);
 		expect(issueClosedTime).to.equal(0);
@@ -41,5 +48,25 @@ describe('OpenQ.sol', () => {
 		expect(issuer).to.equal("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
 		expect(closer).to.equal("0x0000000000000000000000000000000000000000");
 		expect(status).to.equal(0);
+	});
+});
+
+describe('OpenQ.sol Reverts', () => {
+	let openq;
+
+	beforeEach(async () => {
+		const OpenQ = await hre.ethers.getContractFactory('OpenQ');
+		openQ = await OpenQ.deploy();
+	});
+
+	it('mintBounty method should revert if bounty already exists', async () => {
+		// ARRANGE
+		await openQ.deployed();
+
+		// ACT
+		await openQ.mintBounty('mockIssueId');
+
+		// ASSERT
+		await expect(openQ.mintBounty('mockIssueId')).to.be.revertedWith('Issue already exists for given id. Find its address by calling issueToAddress on this contract with the issueId');
 	});
 });
