ffb43ccd47289653185f101ee42863b5b2e0e58b
Flaco <andrew@openq.dev>
2021-12-22 14:06:55 -0500

    adds IOpenQ.sol interface

---
+++ contracts/OpenQ/IOpenQ.sol
@@ -0,0 +1,74 @@
+// contracts/OpenQ.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '../Bounty/Bounty.sol';
+import '../Bounty/Bountyable.sol';
+import '../Helpers/TransferHelper.sol';
+
+interface IOpenQ {
+    // Events
+    event BountyCreated(
+        string bountyId,
+        string organization,
+        address issuerAddress,
+        address bountyAddress,
+        uint256 bountyMintTime
+    );
+
+    event BountyClosed(
+        string bountyId,
+        string organization,
+        address bountyAddress,
+        address payoutAddress,
+        uint256 bountyClosedTime
+    );
+
+    event DepositReceived(
+        string bountyId,
+        string organization,
+        address bountyAddress,
+        address tokenAddress,
+        address sender,
+        uint256 volume,
+        uint256 receiveTime
+    );
+
+    event DepositRefunded(
+        string bountyId,
+        string organization,
+        address bountyAddress,
+        address tokenAddress,
+        address sender,
+        uint256 volume,
+        uint256 refundTime
+    );
+
+    event BountyPaidout(
+        string bountyId,
+        string organization,
+        address bountyAddress,
+        address tokenAddress,
+        address payoutAddress,
+        uint256 volume,
+        uint256 payoutTime
+    );
+
+    function mintBounty(string calldata, string calldata)
+        external
+        returns (address);
+
+    function fundBounty(
+        address,
+        address,
+        uint256
+    ) external returns (bool);
+
+    function claimBounty(string calldata, address) external;
+
+    function refundBountyDeposits(address) external returns (bool);
+
+    function bountyIsOpen(string memory) external view returns (bool);
+
+    function getBountyAddress(string calldata) external view returns (address);
+}
---
+++ contracts/Tokens/MockLink.sol
@@ -0,0 +1,14 @@
+// contracts/MockLink.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract MockLink is ERC20 {
+    address public admin;
+
+    constructor() ERC20('Mock Link', 'mLINK') {
+        _mint(msg.sender, 10000 * 10**18);
+        admin = msg.sender;
+    }
+}
