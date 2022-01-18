928cee298ba18663215c3ef360c8915cf0c5ceff
Flaco <andrew@openq.dev>
2022-01-18 12:45:44 -0600

    adds transfer fee token for testing

---
+++ contracts/Tokens/TransferFeeToken.sol
@@ -0,0 +1,14 @@
+// contracts/MockDai.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+
+contract TokenFeeToken is ERC20 {
+    address public admin;
+
+    constructor() ERC20('Fee Token', 'FEE') {
+        _mint(msg.sender, 10000 * 10**18);
+        admin = msg.sender;
+    }
+}
