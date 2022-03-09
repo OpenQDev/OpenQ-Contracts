333622f60d59e354a80ee4a4f86c7fe4044a24ac
FlacoJones <andrew@openq.dev>
2022-03-09 12:27:37 -0600

    upgrades back up to 0.8.12

--- contracts/Bounty/Bounty.sol
+++ contracts/Bounty/Bounty.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.7;
+pragma solidity 0.8.12;
 
 // Third Party
 import '@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol';
--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = (function () {
 	const chainIdInt = parseInt(chainId);
 
 	const config = {
-		solidity: '0.8.7',
+		solidity: '0.8.12',
 		networks: {
 			localhost: {
 				url: process.env.PROVIDER_URL,
