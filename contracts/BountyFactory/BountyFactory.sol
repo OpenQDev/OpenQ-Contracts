f4ec3e102ccd7425c6bf5f1d4a354df862065fbe
FlacoJones <andrew@openq.dev>
2023-01-23 06:34:48 -0600

    BountyFactory docs

--- contracts/BountyFactory/BountyFactory.sol
+++ contracts/BountyFactory/BountyFactory.sol
@@ -1,42 +1,27 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.17;
 
-/**
- * @dev Third party imports
- */
 import '@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol';
 
-/**
- * @dev Custom imports
- */
 import '../OnlyOpenQ/OnlyOpenQ.sol';
 import '../Library/OpenQDefinitions.sol';
 
-/**
- * @title BountyFactory
- * @dev Factory for deploying new BeaconProxy contracts for bounties. Holds BeaconBounty address passed to each bounty for retrieving their implementation
- */
+/// @title BountyFactory
+/// @author BountyFactory
+/// @notice Factory contract to deploy upgradeable beacon proxies for each type of bounty
 contract BountyFactory is OnlyOpenQ {
-    /**
-     * INITIALIZATION
-     */
-
-    /**
-     * @dev The address of the UpgradeableBeacon holding the current bounty implementation
-     */
+    /// @notice The address of the UpgradeableBeacon holding the current bounty implementation
     address public immutable atomicBountyBeacon;
     address public immutable ongoingBountyBeacon;
     address public immutable tieredPercentageBountyBeacon;
     address public immutable tieredFixedBountyBeacon;
 
-    /**
-     * @dev Deploys and initializes a new BeaconProxy with implementation pulled from BountyBeacon
-     * @param _openQ The OpenQProxy address
-     * @param _atomicBountyBeacon The UpgradeableBeacon "BountyBeacon" address for Atomic contracts
-     * @param _ongoingBountyBeacon The UpgradeableBeacon "BountyBeacon" address for Ongoing contracts
-     * @param _tieredPercentageBountyBeacon The UpgradeableBeacon "BountyBeacon" address for Tiered contracts
-     * @param _tieredFixedBountyBeacon The UpgradeableBeacon "BountyBeacon" address for Tiered Fixed contracts
-     */
+    /// @notice Deploys and initializes a new BeaconProxy with implementation pulled from the appropriate BountyBeacon
+    /// @param _openQ The OpenQProxy address, used to initialize OnlyOpenQ
+    /// @param _atomicBountyBeacon The UpgradeableBeacon "BountyBeacon" address for Atomic contracts
+    /// @param _ongoingBountyBeacon The UpgradeableBeacon "BountyBeacon" address for Ongoing contracts
+    /// @param _tieredPercentageBountyBeacon The UpgradeableBeacon "BountyBeacon" address for Tiered contracts
+    /// @param _tieredFixedBountyBeacon The UpgradeableBeacon "BountyBeacon" address for Tiered Fixed contracts
     constructor(
         address _openQ,
         address _atomicBountyBeacon,
@@ -52,26 +37,23 @@ contract BountyFactory is OnlyOpenQ {
         tieredFixedBountyBeacon = _tieredFixedBountyBeacon;
     }
 
-    /**
-     * TRANSACTIONS
-     */
-
-    /**
-     * @dev Deploys and initializes a new BeaconProxy with implementation pulled from BountyBeacon
-     * @param _id A unique string representing the bounty
-     * @param _issuer The creator of the mint transaction
-     * @param _organization The organization associated with the bounty
-     * @return address The address of the minted bounty
-     */
+    /// @dev Deploys and initializes a new BeaconProxy with implementation pulled from BountyBeacon
+    /// @param _id A UUID representing the bounty's off-chain source (e.g. a Github Issue Id)
+    /// @param _issuer The address of the sender of the the mint transaction
+    /// @param _organization A UUID representing the bounty's off-chain source (e.g. a Github Organization Id)
+    /// @param _claimManager The address of the current ClaimManager. Will be used to initialize onlyClaimManager owned methods in the bounty
+    /// @param _depositManager The address of the current DepositManager. Will be used to initialize onlyDepositManager owned methods in the bounty
+    /// @param _operation ABI Encoded data to be decoded in the initializer of the bounty, with decode type dependent on bounty type
+    /// @return address of the minted bounty
     function mintBounty(
         string memory _id,
         address _issuer,
         string memory _organization,
         address _claimManager,
         address _depositManager,
-        OpenQDefinitions.InitOperation memory operation
+        OpenQDefinitions.InitOperation memory _operation
     ) external onlyOpenQ returns (address) {
-        uint32 operationType = operation.operationType;
+        uint32 operationType = _operation.operationType;
 
         address beaconProxy;
 
@@ -95,7 +77,7 @@ contract BountyFactory is OnlyOpenQ {
                 openQ(),
                 _claimManager,
                 _depositManager,
-                operation
+                _operation
             )
         );
 
