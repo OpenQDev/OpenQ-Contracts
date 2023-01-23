f499340cedb8b26da41caa8ecfd98b56f9156086
FlacoJones <andrew@openq.dev>
2023-01-23 07:15:28 -0600

    refactors mocks to mock folder

--- contracts/Mocks/MockKyc.sol
+++ contracts/Mocks/MockKyc.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.17;
 
-import './IKycValidity.sol';
+import '../KYC/IKycValidity.sol';
 
 contract MockKyc is IKycValidity {
     bool public isValid;
