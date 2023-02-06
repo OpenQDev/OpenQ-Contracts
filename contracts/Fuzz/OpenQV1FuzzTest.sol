bcf65027a55f779e3b14e7004983d8f7da72dc1b
FlacoJones <andrew@openq.dev>
2023-02-06 08:32:39 -0600

    separates Setup

--- contracts/Fuzz/OpenQV1FuzzTest.sol
+++ contracts/Fuzz/OpenQV1FuzzTest.sol
@@ -1,103 +1,16 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.17;
 
-import '../OpenQ/Implementations/OpenQV1.sol';
-import '../OpenQ/Proxy/OpenQProxy.sol';
-import '../BountyFactory/BountyFactory.sol';
-import '../Library/OpenQDefinitions.sol';
-import '../Bounty/Proxy/BountyBeacon.sol';
-import '../Bounty/Implementations/AtomicBountyV1.sol';
-import '../Bounty/Implementations/OngoingBountyV1.sol';
-import '../Bounty/Implementations/TieredPercentageBountyV1.sol';
-import '../Bounty/Implementations/TieredFixedBountyV1.sol';
-import '../ClaimManager/Implementations/ClaimManagerV1.sol';
-import '../DepositManager/Implementations/DepositManagerV1.sol';
-import '../TokenWhitelist/OpenQTokenWhitelist.sol';
+import './Setup.sol';
 
-contract OpenQV1FuzzTest {
-    event DebugBytes(bytes _bytes);
-    event DebugAddress(address _address);
-    event DebugUint256(uint256 _uint256);
+contract OpenQV1FuzzTest is Setup {
+    AtomicBountyV1 atomicBounty;
+    OngoingBountyV1 ongoingBounty;
+    TieredPercentageBountyV1 tieredPercentageBounty;
+    TieredFixedBountyV1 tieredFixedBounty;
 
-    OpenQV1 openQ;
-    ClaimManagerV1 claimManager;
-    DepositManagerV1 depositManager;
+    constructor() {}
 
-    constructor() {
-        // DEPLOY BOUNTY IMPLEMENTATIONS
-        address atomicBountyV1 = address(new AtomicBountyV1());
-        address ongoingBountyV1 = address(new OngoingBountyV1());
-        address tieredPercentageBountyV1 = address(
-            new TieredPercentageBountyV1()
-        );
-        address tieredFixedBountyV1 = address(new TieredFixedBountyV1());
-
-        // DEPLOY BOUNTY BEACONS
-        address atomicBountyBeacon = address(new BountyBeacon(atomicBountyV1));
-        address ongoingBountyBeacon = address(
-            new BountyBeacon(ongoingBountyV1)
-        );
-        address tieredPercentageBountyBeacon = address(
-            new BountyBeacon(tieredPercentageBountyV1)
-        );
-        address tieredFixedBountyBeacon = address(
-            new BountyBeacon(tieredFixedBountyV1)
-        );
-
-        // DEPLOY AND INITIALIZE OPENQ IMPLEMENTATION AND PROXY
-        address openQImplementation = address(new OpenQV1());
-
-        address openQProxy = address(
-            new OpenQProxy(openQImplementation, new bytes(0))
-        );
-
-        openQ = OpenQV1(openQProxy);
-        openQ.initialize();
-
-        // DEPLOY BOUNTY FACTORY
-        address bountyFactory = address(
-            new BountyFactory(
-                address(openQ),
-                atomicBountyBeacon,
-                ongoingBountyBeacon,
-                tieredPercentageBountyBeacon,
-                tieredFixedBountyBeacon
-            )
-        );
-
-        // DEPLOY AND INITIALIZE CLAIM MANAGER
-        address claimManagerImplementation = address(new ClaimManagerV1());
-
-        address claimManagerProxy = address(
-            new OpenQProxy(claimManagerImplementation, new bytes(0))
-        );
-
-        claimManager = ClaimManagerV1(claimManagerProxy);
-        claimManager.initialize(msg.sender);
-
-        // DEPLOY AND INITIALIZE DEPOSIT MANAGER
-        address depositManagerImplementation = address(new DepositManagerV1());
-
-        address depositManagerProxy = address(
-            new OpenQProxy(depositManagerImplementation, new bytes(0))
-        );
-
-        depositManager = DepositManagerV1(depositManagerProxy);
-        depositManager.initialize();
-
-        // DEPLOY OPENQTOKENWHITELIST
-        address openQTokenWhiteList = address(new OpenQTokenWhitelist(5));
-
-        openQ.setBountyFactory(bountyFactory);
-        openQ.setClaimManager(address(claimManager));
-        openQ.setDepositManager(address(depositManager));
-
-        depositManager.setTokenWhitelist(openQTokenWhiteList);
-
-        claimManager.setKyc(address(0));
-    }
-
-    // Will eventually be called in txn sequence
     function assert_mintBounty_atomic(
         bool _hasFundingGoal,
         address _fundingToken,
@@ -121,42 +34,55 @@ contract OpenQV1FuzzTest {
             _alternativeLogo
         );
 
-        address bountyAddress = openQ.mintBounty(
-            '_bountyId',
-            '_organization',
-            OpenQDefinitions.InitOperation(0, initData)
+        (bool success, bytes memory result) = minter.proxy(
+            address(openQ),
+            abi.encodeWithSelector(
+                openQ.mintBounty.selector,
+                'bountyId',
+                '_organization',
+                OpenQDefinitions.InitOperation(0, initData)
+            )
         );
 
-        AtomicBountyV1 atomicBounty = AtomicBountyV1(payable(bountyAddress));
+        require(success);
 
-        emit DebugAddress(bountyAddress);
-        emit DebugUint256(atomicBounty.bountyType());
+        address bountyAddress = abi.decode(result, (address));
+
+        atomicBounty = AtomicBountyV1(payable(bountyAddress));
 
         assert(bountyAddress != address(0x0));
     }
 
-    function assert_test() public {
-        // Pre-condition
-        // Optimization: modular arithmetic
-        // State before action
-        // Action
-        // Post-condition
+    function assert_bounty_issuer_never_changes() public {
+        require(address(atomicBounty) != address(0x0));
+        assert(atomicBounty.issuer() == address(minter));
+    }
 
-        // Use try/catch if you believe that no non-happy path exists, i.e. reverts are impossible
-        try {
-            // Action
-            // Post-condition
-        } catch (bytes memory err) {
-            assert(false);
+    function assert_cannot_transfer_oracle() public {
+        if (!completed) {
+            _init();
         }
+        emit DebugAddress(openQ.oracle());
+        assert(false);
+        require(openQ.oracle() != address(0));
+        assert(openQ.oracle() == address(oracle));
     }
 
-    // bind _number to [0, UPPER_BOUND) (0 inclusive, UPPER_BOUND exclusive)
-    function bind(uint256 _number, uint256 UPPER_BOUND)
-        public
-        pure
-        returns (uint256)
-    {
-        return _number % UPPER_BOUND;
-    }
+    // function assert_test() public {
+    //     // Pre-condition
+    //     // Optimization: modular arithmetic
+    //     // State before action
+    //     // Action
+    //     // Post-condition
+
+    //     // Use try/catch if you believe that no non-happy path exists, i.e. reverts are impossible
+    //     try {
+    //         // Action
+    //         // Post-condition
+    //     } catch (bytes memory err) {
+    //         assert(false);
+    //     }
+    // }
+
+    // assert that something never succeeds with assert(!success) on low-level call
 }
---
+++ contracts/Fuzz/Setup.sol
@@ -0,0 +1,158 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.17;
+
+import '../OpenQ/Implementations/OpenQV1.sol';
+import '../OpenQ/Proxy/OpenQProxy.sol';
+import '../BountyFactory/BountyFactory.sol';
+import '../Library/OpenQDefinitions.sol';
+import '../Bounty/Proxy/BountyBeacon.sol';
+import '../Bounty/Implementations/AtomicBountyV1.sol';
+import '../Bounty/Implementations/OngoingBountyV1.sol';
+import '../Bounty/Implementations/TieredPercentageBountyV1.sol';
+import '../Bounty/Implementations/TieredFixedBountyV1.sol';
+import '../ClaimManager/Implementations/ClaimManagerV1.sol';
+import '../DepositManager/Implementations/DepositManagerV1.sol';
+import '../TokenWhitelist/OpenQTokenWhitelist.sol';
+
+// Use this to get a constant User address
+contract Users {
+    function proxy(address target, bytes memory data)
+        public
+        returns (bool success, bytes memory returnData)
+    {
+        return target.call(data);
+    }
+
+    // (bool success, ) = user.proxy(address(openQ), abi.encodeWithSelector(openQ.mintBounty.selector, ...args));
+
+    // for overloaded overrides
+    // bytes4(keccak256("mint(address)"))
+}
+
+contract Setup {
+    event DebugBytes(bytes _bytes);
+    event DebugAddress(address _address);
+    event DebugUint256(uint256 _uint256);
+
+    OpenQV1 openQ;
+    ClaimManagerV1 claimManager;
+    DepositManagerV1 depositManager;
+
+    bool completed;
+
+    // Simulated EOAs
+    Users owner;
+    Users oracle;
+    Users minter;
+
+    constructor() {
+        // CREATE USER PROXIES (SIMULATES AND EOA)
+        owner = new Users();
+        oracle = new Users();
+        minter = new Users();
+
+        // DEPLOY BOUNTY IMPLEMENTATIONS
+        address atomicBountyV1 = address(new AtomicBountyV1());
+        address ongoingBountyV1 = address(new OngoingBountyV1());
+        address tieredPercentageBountyV1 = address(
+            new TieredPercentageBountyV1()
+        );
+        address tieredFixedBountyV1 = address(new TieredFixedBountyV1());
+
+        // DEPLOY BOUNTY BEACONS
+        address atomicBountyBeacon = address(new BountyBeacon(atomicBountyV1));
+        address ongoingBountyBeacon = address(
+            new BountyBeacon(ongoingBountyV1)
+        );
+        address tieredPercentageBountyBeacon = address(
+            new BountyBeacon(tieredPercentageBountyV1)
+        );
+        address tieredFixedBountyBeacon = address(
+            new BountyBeacon(tieredFixedBountyV1)
+        );
+
+        // DEPLOY AND INITIALIZE OPENQ IMPLEMENTATION AND PROXY
+        address openQImplementation = address(new OpenQV1());
+
+        address openQProxy = address(
+            new OpenQProxy(openQImplementation, new bytes(0))
+        );
+
+        openQ = OpenQV1(openQProxy);
+        openQ.initialize();
+
+        // DEPLOY BOUNTY FACTORY
+        address bountyFactory = address(
+            new BountyFactory(
+                address(openQ),
+                atomicBountyBeacon,
+                ongoingBountyBeacon,
+                tieredPercentageBountyBeacon,
+                tieredFixedBountyBeacon
+            )
+        );
+
+        // DEPLOY AND INITIALIZE CLAIM MANAGER
+        address claimManagerImplementation = address(new ClaimManagerV1());
+
+        address claimManagerProxy = address(
+            new OpenQProxy(claimManagerImplementation, new bytes(0))
+        );
+
+        claimManager = ClaimManagerV1(claimManagerProxy);
+        claimManager.initialize(address(oracle));
+
+        // DEPLOY AND INITIALIZE DEPOSIT MANAGER
+        address depositManagerImplementation = address(new DepositManagerV1());
+
+        address depositManagerProxy = address(
+            new OpenQProxy(depositManagerImplementation, new bytes(0))
+        );
+
+        depositManager = DepositManagerV1(depositManagerProxy);
+        depositManager.initialize();
+
+        // DEPLOY OPENQTOKENWHITELIST
+        address openQTokenWhiteList = address(new OpenQTokenWhitelist(5));
+
+        openQ.setBountyFactory(bountyFactory);
+        openQ.setClaimManager(address(claimManager));
+        openQ.setDepositManager(address(depositManager));
+
+        depositManager.setTokenWhitelist(openQTokenWhiteList);
+
+        claimManager.setKyc(address(0));
+    }
+
+    // function _init() internal {
+    //     completed = true;
+    // }
+
+    // bind _number to [0, UPPER_BOUND) (0 inclusive, UPPER_BOUND exclusive)
+    function bind(uint256 _number, uint256 UPPER_BOUND)
+        public
+        pure
+        returns (uint256)
+    {
+        return _number % UPPER_BOUND;
+    }
+}
+
+// no codepath exists which can lower the bounty's balance of ERC20 or protocol tokens EXCEPT claimBounty, permissionClaimTieredBounty, or refundDeposit
+
+// no codepath exists to change bounty.issuer() after being set in the initializer
+
+// no codepath exists EXCEPT for owner that can call the setBountyFactory, setClaimManager, setDepositManager, transferOracle, or upgradeTo on OpenQ
+// no codepath exists EXCEPT for owner that can call the setOpenQ, upgradeTo, transferOracle, or setKyc on ClaimManager
+// no codepath exists EXCEPT for owner that can call setTokenWhitelist or upgradeTo on DepositManager
+
+// no codepath exists EXCEPT for claimManager that can call claimNft, claimBalance, or close on AtomicBounty
+// no codepath exists EXCEPT for claimManager that can call claimNft or claimOngoingPayout on OngoingBounty
+// no codepath exists EXCEPT for claimManager that can call claimNft, claimTiered or closeCompetition on TieredPercentageBounty
+// no codepath exists EXCEPT for claimManager that can call claimNft, claimTieredFixed or closeCompetition on TieredFixedBounty
+
+// no codepath exists EXCEPT for oracle that can call claimBounty on ClaimManager
+
+// no codepath exists EXCEPT for oracle that can call associateExternalIdToAddress on OpenQ
+
+// no codepath exists EXCEPT for depositManager that can call receiveFunds, refundDeposit or extendDeposit on Bounty
