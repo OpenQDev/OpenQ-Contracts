84adb5602496110448e093d2ecf2f59d9bf63fe1
FlacoJones <andrew@openq.dev>
2023-02-09 15:25:29 -0600

    working encodes

--- Dockerfile
+++ Dockerfile
@@ -14,5 +14,4 @@ CMD curl --connect-timeout 5 \
 	--retry-max-time 40 \
 	'http://ethnode:8545' \
 	&& yarn deploy-contracts:docker \
-	&& yarn configure-whitelist:docker \
-	&& yarn deploy-bounties-fuzz:docker
+	&& yarn configure-whitelist:docker
--- encoder/index.js
+++ encoder/index.js
@@ -2,24 +2,34 @@ const atomicEncode = (hasFundingGoal, fundingGoalTokenAddress, fundingGoalVolume
 	let abiCoder = new ethers.utils.AbiCoder;
 	const initializationSchema = ['bool', 'address', 'uint256' , 'bool' , 'bool', 'bool' , 'string', 'string' , 'string'];
 	const initializationData = [hasFundingGoal, fundingGoalTokenAddress, fundingGoalVolume, invoiceRequired, kycRequired, supportingDocumentsRequired, issuerExternalUserId, alternativeName, alternativeLogo];
+	console.log(abiCoder.encode(initializationSchema, initializationData));
 	return abiCoder.encode(initializationSchema, initializationData);
 };
 
 const ongoingEncode = (payoutTokenAddress, payoutVolume, hasFundingGoal, fundingToken, fundingGoal, invoiceRequired, kycRequired, supportingDocumentsRequired, issuerExternalUserId, alternativeName, alternativeLogo) => {
+	let abiCoder = new ethers.utils.AbiCoder;
 	const initializationSchema = ['address', 'uint256', 'bool', 'address', 'uint256', 'bool', 'bool', 'bool', 'string', 'string', 'string'];
 	const initializationData = [payoutTokenAddress, payoutVolume, hasFundingGoal, fundingToken, fundingGoal, invoiceRequired, kycRequired, supportingDocumentsRequired, issuerExternalUserId, alternativeName, alternativeLogo];
 	return abiCoder.encode(initializationSchema, initializationData);
 };
 
-const tieredPercentageEncode = (payoutSchedule, payoutTokenAddress, invoiceRequired, kycRequired, supportingDocumentsRequired, issuerExternalUserId, alternativeName, alternativeLogo) => {
+const tieredPercentageEncode = (payoutSchedule, hasFundingGoal, fundingToken, fundingGoal, invoiceRequired, kycRequired, supportingDocumentsRequired, issuerExternalUserId, alternativeName, alternativeLogo) => {
+	let abiCoder = new ethers.utils.AbiCoder;
+	
+	const parsedPayoutSchedule = payoutSchedule.replaceAll('"', '').replaceAll('[', '').replaceAll(']', '').split(',').map(el => parseInt(el));
+
 	const initializationSchema = ['uint256[]', 'bool', 'address', 'uint256', 'bool', 'bool', 'bool', 'string', 'string', 'string'];
-	const initializationData = [payoutSchedule, payoutTokenAddress, invoiceRequired, kycRequired, supportingDocumentsRequired, issuerExternalUserId, alternativeName, alternativeLogo];
+	const initializationData = [parsedPayoutSchedule, hasFundingGoal, fundingToken, fundingGoal, invoiceRequired, kycRequired, supportingDocumentsRequired, issuerExternalUserId, alternativeName, alternativeLogo];
 	return abiCoder.encode(initializationSchema, initializationData);
 };
 
-const tieredFixedEncode = (payoutSchedule, hasFundingGoal, fundingToken, fundingGoal, payoutTokenAddress, invoiceRequired, kycRequired, supportingDocumentsRequired, issuerExternalUserId, alternativeName, alternativeLogo) => {
+const tieredFixedEncode = (payoutSchedule, payoutTokenAddress, invoiceRequired, kycRequired, supportingDocumentsRequired, issuerExternalUserId, alternativeName, alternativeLogo) => {
+	let abiCoder = new ethers.utils.AbiCoder;
+
+	const parsedPayoutSchedule = payoutSchedule.replaceAll('"', '').replaceAll('[', '').replaceAll(']', '').split(',').map(el => parseInt(el));
+
 	const initializationSchema = ['uint256[]', 'address', 'bool', 'bool', 'bool', 'string', 'string', 'string'];
-	const initializationData = [payoutSchedule, hasFundingGoal, fundingToken, fundingGoal, payoutTokenAddress, invoiceRequired, kycRequired, supportingDocumentsRequired, issuerExternalUserId, alternativeName, alternativeLogo];
+	const initializationData = [parsedPayoutSchedule, payoutTokenAddress, invoiceRequired, kycRequired, supportingDocumentsRequired, issuerExternalUserId, alternativeName, alternativeLogo];
 	return abiCoder.encode(initializationSchema, initializationData);
 };
 
