92028db46c7e83ac845d6c34bc59657aa3aa01a1
FlacoJones <andrew@openq.dev>
2023-02-25 20:34:49 -0700

    updates OpenQProxy

--- contracts/OpenQ/proxy/OpenQProxy.sol
+++ contracts/OpenQ/proxy/OpenQProxy.sol
@@ -13,7 +13,5 @@ contract OpenQProxy is ERC1967Proxy {
     constructor(address _logic, bytes memory _data)
         payable
         ERC1967Proxy(_logic, _data)
-    {
-        _disableInitializers();
-    }
+    {}
 }
